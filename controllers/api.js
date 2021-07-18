eed3ee2e1f94091026a205bdbd5839c567440652
teradaian <teradaian@gmail.com>
2021-07-18 13:29:24 -0700

    rm index ping w/ redirect

--- controllers/api.js
+++ controllers/api.js
@@ -13,7 +13,7 @@ function index(req, res){
         fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false')
         .then(response => response.json())
         .then(data => {
-            res.render('coins/index', { coins: data, title: 'Coin Index' })
+            res.render('coins/index', { coins: data, title: 'Coin Index', user: req.user ? req.user : null })
         })
     } catch (Error) {
         console.log(Error)
