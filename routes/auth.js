97e18fbcaaa33ec287dc03c1dec84a0865523a34
teradaian <teradaian@gmail.com>
2022-06-28 11:41:27 -0700

    loginUserOnSuccess

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import passport from 'passport'
+import User from './models/user.js'
 
 export {
   router
@@ -12,11 +13,15 @@ router.get(
   passport.authenticate('google', { scope: ['profile', 'email'] })
 )
 
+const loginUserOnSuccess = async(req, res) => {
+  const user = await User.findById(req.user._id)
+  const profile = user.profile
+  res.redirect('/' + profile)
+}
+
 router.get(
   '/google/oauth2callback',
-  passport.authenticate('google', { failureRedirect: '/auth/google' }), function(req, res) {
-    res.redirect('/' + req.user._id)
-  }
+  passport.authenticate('google', { failureRedirect: '/auth/google' }, loginUserOnSuccess)
 )
 
 router.get('/logout', function (req, res) {
